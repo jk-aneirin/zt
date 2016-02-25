b870eb6367b8595d415274f13b09d6122ed77f9d
zt <bigxuliang@sina.com>
2016-02-25 13:37:29 +0800

    add README

--- csvhandler.py
+++ csvhandler.py
@@ -52,7 +52,7 @@ def Getlastrealm(mailbox):
         return mailbox[index:]
 
 def Insertredis(realm):
-## Shell redis-cli -h 192.168.2.182 -p 6379  -a job5156RedisMaster182 -x zadd filter:not_by_hanqi:mail:suffix:list 0
+## Shell redis-cli -h 192.168.2.182 -p 6379  -a passwd -x zadd filter:not_by_hanqi:mail:suffix:list 0
 	r=redis.StrictRedis(host='192.168.2.182',password='job5156RedisMaster182')
 	r.zadd('filter:not_by_hanqi:mail:suffix:list',0,realm)
 	#r=redis.StrictRedis(host='192.168.70.160',port=6379)
